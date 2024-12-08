477ed64132b3374db58c4ba34c5f111aa24c49fb
joao.victor <joaovictormatos705@gmsil.com>
2024-12-08 21:32:13 +0000

    change default flavor to Staging

--- build-logic/convention/src/main/kotlin/enums/Flavor.kt
+++ build-logic/convention/src/main/kotlin/enums/Flavor.kt
@@ -3,7 +3,7 @@ package enums
 internal enum class Flavor {
     DEVELOP, STAGING, PRODUCTION, BBC_NEWS, BBC_SPORT, ALL_NEWS;
 
-    val isDefault get() = this == ALL_NEWS
+    val isDefault get() = this == STAGING
 
     override fun toString() = name.lowercase()
 }
